2b8b7b7ef01b491fcbb6b083b358d7bfde3c0ee0
Can202 <mgoopazo@gmail.com>
2021-04-22 14:37:07 -0400

    tagnav.js

---
+++ docs/src/tagnav.js
@@ -0,0 +1,11 @@
+document.write("<p>");
+document.write("<a href="/OpenSource-GameList">home</a>");
+document.write(" - ");
+document.write("<a href="/OpenSource-GameList/tag/sandbox/">sandbox</a>");
+document.write(" - ");
+document.write("<a href="/OpenSource-GameList/tag/rhythm">rhythm</a>");
+document.write(" - ");
+document.write("<a href="/OpenSource-GameList/tag/strategy">strategy</a>");
+document.write(" - ");
+document.write("<a href="/OpenSource-GameList/tag/simulation">simulation</a>");
+document.write("</p>");
