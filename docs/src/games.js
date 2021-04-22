115b43b2dba4242dc69fbbc69aca953e2add364c
Can202 <mgoopazo@gmail.com>
2021-04-22 15:41:00 -0400

    uuff IDK if this work

---
+++ docs/src/games.js
@@ -0,0 +1,24 @@
+
+if (tag == "sandbox" || tag == "home"){
+    document.write('<li><a href="https://www.minetest.net/" target = "_blank">Minetest</a> - voxel game engine like Minecraft</li>');
+}
+
+if (tag == "rhythm" || tag == "home"){
+    document.write('<li><a href="https://www.stepmania.com/" target = "_blank">Stepmania</a> - dance and rhythm game like Dance Dance Revolution</li>');
+}
+
+if (tag == "rhythm" || tag == "home"){
+    document.write('<li><a href="https://store.steampowered.com/app/980610/Quaver/" target = "_blank">Quaver</a> - competitive rhythm game like Osu Mania</li>');
+}
+
+if (tag == "strategy" || tag == "home"){
+    document.write('<li><a href="https://unknown-horizons.org/" target = "_blank">Unknown Horizons</a> - 2D realtime strategy simulation with an emphasis on economy and city building</li>');
+}
+
+if (tag == "sandbox" || tag == "simulation" || tag == "home"){
+    document.write('<li><a href="https://www.rigsofrods.org/" target = "_blank">Rigs of Rods</a> - vehicle simulator that uses soft-body physics to simulate the motion destruction and deformation of vehicles</li>');
+}
+
+if (tag == "simulation" || tag == "home"){
+    document.write('<li><a href="https://www.flightgear.org/" target="_blank">FlightGear</a> - a sophisticated and professional Flight Simulator</li>');
+}
