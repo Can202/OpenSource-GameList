0a227536879869cc0803700ae0cbf5573646622a
Can202 <mgoopazo@gmail.com>
2021-04-22 16:03:27 -0400

    footer

---
+++ docs/src/footer.js
@@ -0,0 +1,6 @@
+document.write(''+
+'<p>if you want to put a game on the list, <a href="https://github.com/Can202/OpenSource-GameList/issues/new">in this link</a>, put the name, page or repo and a little description</p>'+
+'<a href="https://github.com/Can202/OpenSource-GameList">Repository</a>'+
+'<a href="https://github.com/Can202">Can202(me)</a>'+
+''
+);
