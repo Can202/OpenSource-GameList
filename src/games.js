1aa42745607b2f16a3628666aa928549abdad095
Can202 <mgoopazo@gmail.com>
2021-04-23 18:55:47 -0400

    multiplayer

--- src/games.js
+++ src/games.js
@@ -1,5 +1,5 @@
 
-if (tag == "sandbox" || tag == "multiplayer" || tag == "android" || tag == "home"){
+if (tag == "sandbox" || tag == "local_multiplayer" || tag == "online_multiplayer" || tag == "android" || tag == "home"){
     document.write('<li><a href="https://www.minetest.net/" target = "_blank">Minetest</a> - voxel game engine like Minecraft');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
@@ -8,7 +8,7 @@ if (tag == "sandbox" || tag == "multiplayer" || tag == "android" || tag == "home
     document.write('</li>');
 }
 
-if (tag == "rhythm" || tag == "multiplayer" || tag == "home"){
+if (tag == "rhythm" || tag == "on_device_multiplayer" || tag == "home"){
     document.write('<li><a href="https://www.stepmania.com/" target = "_blank">Stepmania</a> - dance and rhythm game like Dance Dance Revolution');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
@@ -17,7 +17,7 @@ if (tag == "rhythm" || tag == "multiplayer" || tag == "home"){
     document.write('</li>');
 }
 
-if (tag == "rhythm" || tag == "multiplayer" || tag == "home"){
+if (tag == "rhythm" || tag == "online_multiplayer" || tag == "home"){
     document.write('<li><a href="https://store.steampowered.com/app/980610/Quaver/" target = "_blank">Quaver</a> - competitive rhythm game like Osu Mania');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
@@ -26,7 +26,7 @@ if (tag == "rhythm" || tag == "multiplayer" || tag == "home"){
     document.write('</li>');
 }
 
-if (tag == "strategy" || tag == "multiplayer" || tag == "home"){
+if (tag == "strategy" || tag == "online_multiplayer" || tag == "home"){
     document.write('<li><a href="https://unknown-horizons.org/" target = "_blank">Unknown Horizons</a> - 2D realtime strategy simulation with an emphasis on economy and city building');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
@@ -35,13 +35,13 @@ if (tag == "strategy" || tag == "multiplayer" || tag == "home"){
     document.write('</li>');
 }
 
-if (tag == "casual" || tag == "multiplayer" || tag == "android" || tag == "home"){
+if (tag == "casual" || tag == "on_device_multiplayer" || tag == "android" || tag == "home"){
     document.write('<li><a href="https://can202.github.io/arar/" target="_blank">Arar</a> - two players in one device, the winner is that manage to insert more rings');
     document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
     document.write('</li>');
 }
 
-if (tag == "sandbox" || tag == "simulation" || tag == "multiplayer" || tag == "home"){
+if (tag == "sandbox" || tag == "simulation" || tag == "online_multiplayer" || tag == "local_multiplayer" || tag == "home"){
     document.write('<li><a href="https://www.rigsofrods.org/" target = "_blank">Rigs of Rods</a> - vehicle simulator that uses soft-body physics to simulate the motion destruction and deformation of vehicles');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
@@ -68,7 +68,7 @@ if (tag == "plataformer" || tag == "android" || tag == "home"){
     document.write('</li>');
 }
 
-if (tag == "racing" || tag == "multiplayer" || tag == "android" || tag == "home"){
+if (tag == "racing" || tag == "on_device_multiplayer" || tag == "online_multiplayer" || tag == "local_multiplayer" || tag == "android" || tag == "home"){
     document.write('<li><a href="https://supertuxkart.net/" target="_blank">SuperTuxKart</a> - 3D arcade racer with a variety characters, tracks, and modes to play');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
@@ -77,7 +77,7 @@ if (tag == "racing" || tag == "multiplayer" || tag == "android" || tag == "home"
     document.write('</li>');
 }
 
-if (tag == "minigames" || tag == "multiplayer" || tag == "home"){
+if (tag == "minigames" || tag == "on_device_multiplayer" || tag == "home"){
     document.write('<li><a href="https://supertux.party/" target="_blank">SuperTuxParty</a> - Minigames to play with friends on a virtual board like Mario Party');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
@@ -86,7 +86,7 @@ if (tag == "minigames" || tag == "multiplayer" || tag == "home"){
     document.write('</li>');
 }
 
-if (tag == "strategy" || tag == "multiplayer" || tag == "android" || tag == "home"){
+if (tag == "strategy" || tag == "online_multiplayer" || tag == "local_multiplayer" || tag == "android" || tag == "home"){
     document.write('<li><a href="https://store.steampowered.com/app/1536610/OpenTTD/" target="_blank">OpenTTD</a> - simulation game based upon Transport Tycoon Deluxe');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
@@ -95,7 +95,7 @@ if (tag == "strategy" || tag == "multiplayer" || tag == "android" || tag == "hom
     document.write('</li>');
 }
 
-if (tag == "shooter" || tag == "multiplayer" || tag == "home"){
+if (tag == "shooter" || tag == "online_multiplayer" || tag == "local_multiplayer" || tag == "home"){
     document.write('<li><a href="https://www.urbanterror.info/home/" target="_blank">Urban Terror</a> - free multiplayer first person shooter');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
@@ -104,7 +104,7 @@ if (tag == "shooter" || tag == "multiplayer" || tag == "home"){
     document.write('</li>');
 }
 
-if (tag == "shooter" || tag == "multiplayer" || tag == "home"){
+if (tag == "shooter" || tag == "online_multiplayer" || tag == "local_multiplayer" || tag == "home"){
     document.write('<li><a href="https://assault.cubers.net/" target="_blank">AssaultCube</a> - free multiplayer first person shooter');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
@@ -113,7 +113,7 @@ if (tag == "shooter" || tag == "multiplayer" || tag == "home"){
     document.write('</li>');
 }
 
-if (tag == "shooter" || tag == "multiplayer" || tag == "android" || tag == "home"){
+if (tag == "shooter" || tag == "local_multiplayer" || tag == "android" || tag == "home"){
     document.write('<li><a href="http://www.openarena.ws/download.php" target="_blank">OpenArena</a> - deathmatch first person shooter');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
