e8e25d0ff245b34dd9a8fd71b6e8e45ffed0688e
Can202 <mgoopazo@gmail.com>
2021-04-23 17:47:44 -0400

    system and AC

--- src/games.js
+++ src/games.js
@@ -1,44 +1,108 @@
 
 if (tag == "sandbox" || tag == "home"){
-    document.write('<li><a href="https://www.minetest.net/" target = "_blank">Minetest</a> - voxel game engine like Minecraft</li>');
+    document.write('<li><a href="https://www.minetest.net/" target = "_blank">Minetest</a> - voxel game engine like Minecraft');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('</li>');
 }
 
 if (tag == "rhythm" || tag == "home"){
-    document.write('<li><a href="https://www.stepmania.com/" target = "_blank">Stepmania</a> - dance and rhythm game like Dance Dance Revolution</li>');
+    document.write('<li><a href="https://www.stepmania.com/" target = "_blank">Stepmania</a> - dance and rhythm game like Dance Dance Revolution');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    //document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('</li>');
 }
 
 if (tag == "rhythm" || tag == "home"){
-    document.write('<li><a href="https://store.steampowered.com/app/980610/Quaver/" target = "_blank">Quaver</a> - competitive rhythm game like Osu Mania</li>');
+    document.write('<li><a href="https://store.steampowered.com/app/980610/Quaver/" target = "_blank">Quaver</a> - competitive rhythm game like Osu Mania');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    //document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('</li>');
 }
 
 if (tag == "strategy" || tag == "home"){
-    document.write('<li><a href="https://unknown-horizons.org/" target = "_blank">Unknown Horizons</a> - 2D realtime strategy simulation with an emphasis on economy and city building</li>');
+    document.write('<li><a href="https://unknown-horizons.org/" target = "_blank">Unknown Horizons</a> - 2D realtime strategy simulation with an emphasis on economy and city building');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    //document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    //document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('</li>');
 }
 
 if (tag == "sandbox" || tag == "simulation" || tag == "home"){
-    document.write('<li><a href="https://www.rigsofrods.org/" target = "_blank">Rigs of Rods</a> - vehicle simulator that uses soft-body physics to simulate the motion destruction and deformation of vehicles</li>');
+    document.write('<li><a href="https://www.rigsofrods.org/" target = "_blank">Rigs of Rods</a> - vehicle simulator that uses soft-body physics to simulate the motion destruction and deformation of vehicles');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    //document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    //document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('</li>');
 }
 
 if (tag == "simulation" || tag == "home"){
-    document.write('<li><a href="https://www.flightgear.org/" target="_blank">FlightGear</a> - a sophisticated and professional Flight Simulator</li>');
+    document.write('<li><a href="https://www.flightgear.org/" target="_blank">FlightGear</a> - a sophisticated and professional Flight Simulator');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    //document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('</li>');
 }
 
 if (tag == "plataformer" || tag == "home"){
-    document.write('<li><a href="https://www.supertux.org/" target="_blank">SuperTux</a> - 2D jump and run sidescroller game in a style similar to the original Super Mario games</li>');
+    document.write('<li><a href="https://www.supertux.org/" target="_blank">SuperTux</a> - 2D jump and run sidescroller game in a style similar to the original Super Mario games');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('</li>');
 }
 
 if (tag == "racing" || tag == "home"){
-    document.write('<li><a href="https://supertuxkart.net/" target="_blank">SuperTuxKart</a> - 3D arcade racer with a variety characters, tracks, and modes to play</li>');
+    document.write('<li><a href="https://supertuxkart.net/" target="_blank">SuperTuxKart</a> - 3D arcade racer with a variety characters, tracks, and modes to play');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('</li>');
 }
 
 if (tag == "minigames" || tag == "home"){
-    document.write('<li><a href="https://supertux.party/" target="_blank">SuperTuxParty</a> - Minigames to play with friends on a virtual board like Mario Party</li>');
+    document.write('<li><a href="https://supertux.party/" target="_blank">SuperTuxParty</a> - Minigames to play with friends on a virtual board like Mario Party');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    //document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('</li>');
 }
 
 if (tag == "strategy" || tag == "home"){
-    document.write('<li><a href="https://store.steampowered.com/app/1536610/OpenTTD/" target="_blank">OpenTTD</a> - simulation game based upon Transport Tycoon Deluxe</li>');
+    document.write('<li><a href="https://store.steampowered.com/app/1536610/OpenTTD/" target="_blank">OpenTTD</a> - simulation game based upon Transport Tycoon Deluxe');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    document.write('<a href="https://play.google.com/store/apps/details?id=org.openttd.sdl&hl=en_US&gl=US" target="_blank"><img src="/OpenSource-GameList/src/images/android.png" width="15px" />.</a>'); //android
+    document.write('</li>');
 }
 
 if (tag == "shooter" || tag == "home"){
-    document.write('<li><a href="https://www.urbanterror.info/home/" target="_blank">Urban Terror</a> - free multiplayer first person shooter</li>');
+    document.write('<li><a href="https://www.urbanterror.info/home/" target="_blank">Urban Terror</a> - free multiplayer first person shooter');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    //document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('</li>');
+}
+
+if (tag == "shooter" || tag == "home"){
+    document.write('<li><a href="https://assault.cubers.net/" target="_blank">AssaultCube</a> - free multiplayer first person shooter');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    //document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('</li>');
 }
