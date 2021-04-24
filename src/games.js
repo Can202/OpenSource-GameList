24f5199bf87eab0b0eed88780cb65be9403f479b
Can202 <mgoopazo@gmail.com>
2021-04-23 20:21:15 -0400

    OpenSurge

--- src/games.js
+++ src/games.js
@@ -156,6 +156,15 @@ if (tag == "rhythm" || tag == "android" || tag == "home"){
     document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
     document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
     document.write('</li>');
+}
+
+if (tag == "plataformer" || tag == "casual" || tag == "home"){
+    document.write('<li><a href="https://opensurge2d.org/" target="_blank">Open Surge</a> - 2D retro platformer inspired by Sonic games and a game creation system');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    //document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('</li>');
+}
 
 document.write('<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>');
-}
