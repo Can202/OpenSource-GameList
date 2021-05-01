9866077040c3c6e52bcee3cf84a0db3fcb017db7
Can202 <mgoopazo@gmail.com>
2021-05-01 03:34:45 -0400

    form

--- src/footer.js
+++ src/footer.js
@@ -1,5 +1,5 @@
 document.write(''+
-'<p>if you want to put a game on the list, <a href="https://github.com/Can202/OpenSource-GameList/issues/new" target="_blank">in this link</a>, put the name, page or repo and a little description</p>'+
+'<p>if you want to put a game on the list, <a href="https://can202.github.io/OpenSource-GameList/other/form/" target="_blank">in this link</a>, put the name, page or repo and a little description</p>'+
 '<p>I decide if I put it, or not (can be your game or any game, if it is Open Source) - '+
 '<a href="https://github.com/Can202/OpenSource-GameList" target="_blank">Repository</a> - '+
 '<a href="https://github.com/Can202" target="_blank">Can202(me)</a></p>'+
