4afcd3e53f305da207b464d97674995c1218cfe7
Can202 <mgoopazo@gmail.com>
2021-05-02 00:31:49 -0400

    comment

--- src/footer.js
+++ src/footer.js
@@ -3,6 +3,6 @@ document.write(''+
 '<p>I decide if I put it, or not (can be your game or any game, if it is Open Source) - '+
 '<a href="https://github.com/Can202/OpenSource-GameList" target="_blank">Repository</a> - '+
 '<a href="https://github.com/Can202" target="_blank">Can202(me)</a> - '+
-'<a href="https://can202.github.io/OpenSource-GameList/other/comments/caveat" target="_blank">Comment section</a></p>'+
+'<a href="https://can202.github.io/OpenSource-GameList/other/comments/caveat">Comment section</a></p>'+
 '<p>If you want to be an administrator (open source games finder), fork it and send me</p>'
 );
