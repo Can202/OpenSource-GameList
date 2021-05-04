b2a0f9a49f6859d59d8a4bae0b67f5fa6e043dd2
Can202 <mgoopazo@gmail.com>
2021-05-04 14:20:14 -0400

    Warfork

--- src/games.js
+++ src/games.js
@@ -177,6 +177,17 @@ if (tag == "action" || tag == "shooter" || tag == "online_multiplayer" || tag ==
 
 if (tag == "action" || tag == "shooter" || tag == "online_multiplayer" || tag == "home"){
     total = total + 1;
+    document.write('<li><a href="https://store.steampowered.com/app/671610/Warfork/" target="_blank">Warfork</a> - (fork of Warsow)futuristic cartoonish world fast-paced first-person shooter');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    //document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('<font style="font-size:10px;"> - action, shooter, online_multiplayer</font>');
+    document.write('</li>');
+}
+
+if (tag == "action" || tag == "shooter" || tag == "online_multiplayer" || tag == "home"){
+    total = total + 1;
     document.write('<li><a href="https://www.warsow.net/" target="_blank">Warsow</a> - futuristic cartoonish world fast-paced first-person shooter ');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
