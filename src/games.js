4730d1215c2fc2bf10b547732935e467ef57a579
Can202 <mgoopazo@gmail.com>
2021-05-05 10:00:50 -0400

    Teeworlds page

--- src/games.js
+++ src/games.js
@@ -221,7 +221,7 @@ if (tag == "action" || tag == "shooter" || tag == "online_multiplayer" || tag ==
 
 if (tag == "action" || tag == "shooter" || tag == "online_multiplayer" || tag == "home"){
     total = total + 1;
-    document.write('<li><a href="https://store.steampowered.com/app/380840/Teeworlds/" target="_blank">Teeworlds</a> - A retro 2D multiplayer shooter');
+    document.write('<li><a href="https://can202.github.io/OpenSource-GameList/games/Teeworlds">Teeworlds</a> - A retro 2D multiplayer shooter');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
     document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
