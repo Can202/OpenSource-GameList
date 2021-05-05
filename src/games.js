6b69184548f60428fff78e01209d20f11bfba688
Can202 <mgoopazo@gmail.com>
2021-05-05 09:28:13 -0400

    OpenRA

--- src/games.js
+++ src/games.js
@@ -230,6 +230,17 @@ if (tag == "action" || tag == "shooter" || tag == "online_multiplayer" || tag ==
     document.write('</li>');
 }
 
+if (tag == "strategy" || tag == "online_multiplayer" || tag == "action" || tag == "simulation" || tag == "home"){
+    total = total + 1;
+    document.write('<li><a href="https://can202.github.io/OpenSource-GameList/games/OpenRA">OpenRA</a> - classic Command & Conquer real time strategy game');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    //document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('<font style="font-size:10px;"> - strategy, online_multiplayer, action, simulation</font>');
+    document.write('</li>');
+}
+
 if (tag == "action" || tag == "shooter" || tag == "online_multiplayer" || tag == "home"){
     total = total + 1;
     document.write('<li><a href="https://www.urbanterror.info/home/" target="_blank">Urban Terror</a> - free multiplayer first person shooter');
