eb49b4dda7f8383b57de7c397124b7c4daa6f5e7
Can202 <mgoopazo@gmail.com>
2021-05-06 09:07:18 -0400

    ExtremeTuxRacer

--- src/games.js
+++ src/games.js
@@ -519,6 +519,17 @@ if (tag == "action" || tag == "shooter" || tag == "online_multiplayer" ||  tag =
     document.write('</li>');
 }
 
+if (tag == "action" || tag == "racing" || tag == "casual" || tag == "home"){
+    total = total + 1;
+    document.write('<li><a href="https://can202.github.io/OpenSource-GameList/games/ExtremeTuxRacer"> - High speed arctic racing game based on Tux Racer');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    //document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    //document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('<font style="font-size:10px;"> - action, casual, racing</font>');
+    document.write('</li>');
+}
+
 if (tag == "casual" || tag == "android" || tag == "puzzle" || tag == "home"){
     total = total + 1;
     document.write('<li><a href="https://github.com/ogarcia/opensudoku" target="_blank">Open Sudoku</a> - simple open-source android Sudoku game (Fdroid and PlayStore)');
