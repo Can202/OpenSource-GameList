c9230438679e1c6e6174ef3f99518e21987dda49
Can202 <mgoopazo@gmail.com>
2021-05-05 20:18:31 -0400

    updatepage

--- src/games.js
+++ src/games.js
@@ -263,14 +263,14 @@ if (tag == "action" || tag == "shooter" || tag == "online_multiplayer" || tag ==
     document.write('</li>');
 }
 
-if (tag == "action" || tag == "shooter" || tag == "local_multiplayer" || tag == "android" || tag == "home"){
+if (tag == "action" || tag == "shooter" || tag == "local_multiplayer" || tag == "online_multiplayer" || tag == "android" || tag == "home"){
     total = total + 1;
     document.write('<li><a href="https://can202.github.io/OpenSource-GameList/games/OpenArena">OpenArena</a> - deathmatch first person shooter');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
     document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
     document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
-    document.write('<font style="font-size:10px;"> - action, shooter, local_multiplayer, android</font>');
+    document.write('<font style="font-size:10px;"> - action, shooter, local_multiplayer, online_multiplayer, android</font>');
     document.write('</li>');
 }
 
