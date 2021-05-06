b012a7d37cde534582dd0fdc7253baf4db8e2cbf
Can202 <mgoopazo@gmail.com>
2021-05-06 11:44:58 -0400

    AC page

--- src/games.js
+++ src/games.js
@@ -70,7 +70,7 @@ if (tag == "action" || tag == "rhythm" || tag == "on_device_multiplayer" || tag 
 
 if (tag == "action" || tag == "shooter" || tag == "online_multiplayer" || tag == "local_multiplayer" || tag == "home"){
     total = total + 1;
-    document.write('<li><a href="https://assault.cubers.net/" target="_blank">AssaultCube</a> - free multiplayer first person shooter like CS 1.6');
+    document.write('<li><a href="https://can202.github.io/OpenSource-GameList/games/AssaultCube">AssaultCube</a> - free multiplayer first person shooter like CS 1.6');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
     document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
