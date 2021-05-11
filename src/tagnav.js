cc33bfac4ace3c67908cc6d2c8a4fb1e50e4c408
Can202 <mgoopazo@gmail.com>
2021-05-11 17:54:52 -0400

    metroidvania tag

--- src/tagnav.js
+++ src/tagnav.js
@@ -28,6 +28,8 @@ document.write('<a href="/OpenSource-GameList/tag/rhythm">rhythm</a>');
 document.write(' - ');
 document.write('<a href="/OpenSource-GameList/tag/strategy">strategy</a>');
 document.write(' - ');
+document.write('<a href="/OpenSource-GameList/tag/metroidvania">metroidvania</a>');
+document.write(' - ');
 document.write('<a href="/OpenSource-GameList/tag/simulation">simulation</a>');
 document.write(' - ');
 document.write('<a href="/OpenSource-GameList/tag/puzzle">puzzle</a>');
