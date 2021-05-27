8e5489f134c92d0bce296d199e1f70bdb2853bcb
Can202 <mgoopazo@gmail.com>
2021-05-27 16:14:33 -0400

    DDNet

--- src/games.js
+++ src/games.js
@@ -385,6 +385,16 @@ if (tag == "rpg" || tag == "action" || tag == "home"){
     document.write('</li>');
 }
 
+if (tag == "action" || tag == "racing" || tag == "online_multiplayer" || tag == "home"){
+    total = total + 1;
+    document.write('<li><a href="https://can202.github.io/OpenSource-GameList/games/DDNet">DDNet</a> - cooperative racing mod of Teeworlds');
+    document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
+    document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
+    document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
+    //document.write('<img src="/OpenSource-GameList/src/images/android.png" width="15px" />'); //android
+    document.write('<font style="font-size:10px;"> - action, racing, online_multiplayer</font>');
+    document.write('</li>');
+}
 
 if (tag == "simulation" || tag == "home"){
     total = total + 1;
