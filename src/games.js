d5f304ba7e373e4559ab6f5c0343aa5bd80525b3
Can202 <mgoopazo@gmail.com>
2021-05-30 22:10:54 -0400

    UrT page

--- src/games.js
+++ src/games.js
@@ -319,7 +319,7 @@ if (tag == "strategy" || tag == "online_multiplayer" || tag == "action" || tag =
 
 if (tag == "action" || tag == "shooter" || tag == "online_multiplayer" || tag == "home"){
     total = total + 1;
-    document.write('<li><a href="https://www.urbanterror.info/home/" target="_blank">Urban Terror</a> - free multiplayer first person shooter');
+    document.write('<li><a href="https://can202.github.io/OpenSource-GameList/games/UrbanTerror">Urban Terror</a> - free multiplayer first person shooter');
     document.write('<img src="/OpenSource-GameList/src/images/windows.png" width="15px" />'); //windows
     document.write('<img src="/OpenSource-GameList/src/images/linux.png" width="15px" />'); //linux
     document.write('<img src="/OpenSource-GameList/src/images/mac.png" width="15px" />'); //mac
