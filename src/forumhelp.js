2842c814e2e46f4cef8e05711998764719c28e60
Can202 <mgoopazo@gmail.com>
2021-06-08 18:34:25 -0400

    forum help

---
+++ src/forumhelp.js
@@ -0,0 +1,1 @@
+document.write('<p align="center">Problems with this game??, look for help in the <a href="https://github.com/Can202/OpenSource-GameList/discussions/categories/help-with-this-game" target="_blank">forum.</a></p>')
